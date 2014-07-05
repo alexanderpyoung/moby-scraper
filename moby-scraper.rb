d711f8b467e6804eb8c9b274b225f3fab3364d0e
Alexander Young <alexander@lxndryng.com>
2014-07-05 12:23:13 +0100

    FONDLED THE CODE

--- moby-scraper.rb
+++ moby-scraper.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 module MobyScraper
     class MobyScraper
 
-        attr_reader :publisher, :developer, :released, :platform
+        attr_reader :publisher, :developer, :released, :platform, :cover_end_url
         attr_reader :cover_abs_url, :genre, :perspective, :theme_csv
 
         def initialize(game)
@@ -23,11 +23,11 @@ module MobyScraper
 
                 cover_rel_url = mg_page.css('#coreGameCover img').first['src']
                 cover_split_url = cover_rel_url.split('/')
-                cover_end_url = cover_split_url[cover_split_url.length - 1]
+                @cover_end_url = cover_split_url[cover_split_url.length - 1]
                 if cover_rel_url.include? '/shots/'
-                    @cover_abs_url = 'http://www.mobygames.com/images/shots/l/' + cover_end_url
+                    @cover_abs_url = 'http://www.mobygames.com/images/shots/l/' + @cover_end_url
                 else
-                    @cover_abs_url = 'http://www.mobygames.com/images/covers/large/' + cover_end_url
+                    @cover_abs_url = 'http://www.mobygames.com/images/covers/large/' + @cover_end_url
                 end
 
                 @genre = mg_page.css('#coreGameGenre div div')[1].text
@@ -37,6 +37,13 @@ module MobyScraper
             else
                 raise 'The game cannot be found in MD. This game may not exist.'
             end
+
+          def save_image
+            image_data = open(self.cover_abs_url).read
+            destination = File.open(self.cover_end_url, 'wb')
+            destination.write(image_data)
+            return true
+          end
         end
     end
 end
@@ -50,3 +57,4 @@ puts result.cover_abs_url
 puts result.genre
 puts result.perspective
 puts result.theme_csv
+puts result.save_image
