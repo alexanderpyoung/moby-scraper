ca01b2999631aec4591ce2f912a1d37bb4336f14
Alexander Young <alexander@lxndryng.com>
2015-03-11 20:23:25 +0000

    Reorganisation, breaking up of function into smaller functions. Saner way to deal with inconsistent data quality from MobyGames.

--- lib/moby-scraper/moby-scraper.rb
+++ lib/moby-scraper/moby-scraper.rb
@@ -2,55 +2,59 @@ require 'open-uri'
 require 'nokogiri'
 
 module MobyScraper
-    class MobyScraper
-        attr_reader :publisher, :developer, :released, :platforms, :cover_end_url
-        attr_reader :cover_abs_url, :genre, :perspective, :theme_csv, :game_name
-
-        def initialize(game)
-          nbsp_nokogiri = Nokogiri::HTML('&nbsp;').text
-
-          @game_name = game  
-          mg_page = Nokogiri::HTML(open("http://www.mobygames.com/game/#{game}"))
-
-            if !mg_page.at_css('title').text.include? '{gameTitle}'
-              # the publishers and developers have nbsps in their names. Nokogiri to_ses these literally.
-              # the gsub replaces the nbsp with normal spaces 
-
-              @publisher = mg_page.css('#coreGameRelease div')[1].text.gsub(nbsp_nokogiri, " ")
-              @developer = mg_page.css('#coreGameRelease div')[3].text.gsub(nbsp_nokogiri, " ").split(", ")
-                
-              @released = mg_page.css('#coreGameRelease div')[5].text
-              @platforms = mg_page.css('#coreGameRelease div')[7].text.split(", ")
-
-              # the image URLs on MG are relative to domain root. Some light string
-              # manipulation here to deal with this, and to get the larger image.
-              # we also need to differentiate between games with cover images and games with
-              # stand-in cover images.
-
-              cover_rel_url = mg_page.css('#coreGameCover img').first['src']
-              cover_split_url = cover_rel_url.split('/')
-              @cover_end_url = cover_split_url[cover_split_url.length - 1]
-              if cover_rel_url.include? '/shots/'
-                  @cover_abs_url = 'http://www.mobygames.com/images/shots/l/' + @cover_end_url
-              else
-                  @cover_abs_url = 'http://www.mobygames.com/images/covers/large/' + @cover_end_url
-              end
-
-              @genre = mg_page.css('#coreGameGenre div div')[1].text
-              @perspective = mg_page.css('#coreGameGenre div div')[3].text
-              @theme_csv = mg_page.css('#coreGameGenre div div')[5].text.split(", ")
-
-          else
-              raise ArgumentError, 'The game cannot be found in MD. This game may not exist.'
-          end
-
-          def save_image
-            image_data = open(self.cover_abs_url).read
-            destination = File.open(self.cover_end_url, 'wb')
-            destination.write(image_data)
-            return true
-          end
-        end
+  class MobyScraper
+    def initialize(game)
+      @mg_page = Nokogiri::HTML(open("http://www.mobygames.com/game/#{game}"))
+      if @mg_page.at_css('title').text.include? '{gameTitle}'
+        raise ArgumentError, 'The game cannot be found in MD. This game may not exist.'
+      end
     end
-end
+        
+    def get_meta
+      
+      # just in case, we can use this to replace nbsps with proper spaces
+      nbsp_nokogiri = Nokogiri::HTML('&nbsp;').text
+      
+      meta = Hash.new
+      css_selectors = ['#coreGameRelease div', '#coreGameGenre div div']
+      for selector in css_selectors
+        lhs_meta = @mg_page.css(selector)
+        for number in (0..lhs_meta.length - 1).step(2)
+          symbol = lhs_meta[number].text.gsub(" ", "_").downcase.to_sym
+          
+          # we will want some of our values to be arrays of things (themes, genres etc)
+          # but not all, obviously
 
+          value_initial = lhs_meta[number + 1].text.split(", ")
+          if value_initial.length == 1
+            value_keep = value_initial[0]
+          else
+            value_keep = value_initial
+          end
+          meta[symbol] = value_keep
+        end
+      end
+      return meta
+    end
+
+    def get_cover
+      cover_rel_url = @mg_page.at_css("#coreGameCover img")['src']
+      cover_split_url = cover_rel_url.split('/')
+      cover_end_url = cover_split_url.last
+      
+      # sometimes we have screenshots, sometimes cover art
+      
+      if cover_rel_url.include? '/shots/'
+        cover_abs_url = 'http://mobygames.com/images/shots/l/' + cover_end_url
+      else
+        cover_abs_url = 'http://mobygames.com/images/covers/large/' + cover_end_url
+      end
+      return cover_abs_url
+    end
+
+    def fetch_image
+      image_data = open(self.get_cover)
+      return image_data
+    end
+  end
+end
--- tests/test_moby-scraper.rb
+++ tests/test_moby-scraper.rb
@@ -1,27 +1,32 @@
 require 'moby-scraper'
 require 'test/unit'
+require 'test_helper'
 
 class TestMoby < Test::Unit::TestCase
-  def test_fetch_k7()
+  def test_fetch_k7
     game = MobyScraper::MobyScraper.new('killer7')
-    assert(game.publisher.include? "Capcom")
-    assert(game.developer.length == 2)
-    assert(game.developer[0].include? "Capcom")
-    assert(game.developer[1].include? "grasshopper")
+    game_meta = game.get_meta
+    assert(game_meta[:published_by].include? "Capcom")
+    assert(game_meta[:developed_by].length == 2)
+    assert(game_meta[:theme].length == 2)
+    assert(game_meta[:theme].include? "Puzzle-Solving")
   end
 
-  def test_fetch_007()
-    game = MobyScraper::MobyScraper.new('007-agent-under-fire')
-    assert_equal(game.publisher, "Electronic Arts, Inc.")
-    assert(game.developer.length == 2)
-    assert_equal(game.developer[0], "Electronic Arts Canada")
-    assert_equal(game.released, "Nov 15, 2001")
-    assert(game.platforms.length == 3)
-  end
-
-  def test_exception()
+  def test_exception
     assert_raise ArgumentError do
       game = MobyScraper::MobyScraper.new("nonsense-string-probably-not-a-game")
     end
+  end
+
+  def test_cover
+    game = MobyScraper::MobyScraper.new('killer7')
+    cover = game.get_cover
+    assert_equal(cover, "http://mobygames.com/images/covers/large/1141368072-00.jpg")
+  end
+
+  def test_shots
+    game = MobyScraper::MobyScraper.new('atomic-protector')
+    cover = game.get_cover
+    assert_equal(cover, "http://mobygames.com/images/shots/l/649401-atomic-protector-bbc-micro-screenshot-title-screens.jpg")
   end
 end
